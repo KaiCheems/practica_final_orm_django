bef335e3de3a90be2216a45a6a69c6790125c48d
Mauro <mauricio,varas2@gmail.com>
2025-01-24 17:52:11 -0300

    Implementación de modelos para laboratorio

--- practica_final_orm_django/laboratorio/models.py
+++ practica_final_orm_django/laboratorio/models.py
@@ -1,23 +1,21 @@
 from django.db import models
 
 class Laboratorio(models.Model):
-    nombre = models.CharField(max_length=255, unique=True)
+    nombre = models.CharField(max_length=100)
 
     def __str__(self):
         return self.nombre
-
 
 class DirectorGeneral(models.Model):
-    nombre = models.CharField(max_length=255)
-    laboratorio = models.OneToOneField(Laboratorio, on_delete=models.CASCADE, related_name='director')
+    nombre = models.CharField(max_length=100)
+    laboratorio = models.OneToOneField(Laboratorio, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.nombre
 
-
 class Producto(models.Model):
-    nombre = models.CharField(max_length=255)
-    laboratorio = models.ForeignKey(Laboratorio, on_delete=models.CASCADE, related_name='productos')
+    nombre = models.CharField(max_length=100)
+    laboratorio = models.ForeignKey(Laboratorio, on_delete=models.CASCADE)
     f_fabricacion = models.DateField()
     p_costo = models.DecimalField(max_digits=12, decimal_places=2)
     p_venta = models.DecimalField(max_digits=12, decimal_places=2)
