8677a7b377e602068a1c63e3cfaf2a105cd0177b
Mauro <mauricio,varas2@gmail.com>
2025-01-24 16:57:50 -0300

    Configuración inicial del proyecto

---
+++ practica_final_orm_django/ventas/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from clientes.models import Cliente
+from productos.models import Producto
+
+class Venta(models.Model):
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+    producto = models.ForeignKey(Producto, on_delete=models.CASCADE)
+    cantidad = models.PositiveIntegerField()
+    fecha = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Venta de {self.producto} a {self.cliente}"
